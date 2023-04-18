76048b8e6a74c5ce7eb7a5fc95298a907c898ea0
Kyer Potts <kyerpotts@gmail.com>
2023-04-18 16:53:28 +0800

    Finished customizing my useful plugins - TODO: Configure DAP for java

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,16 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  -- TODO: find out how to set background to transparent for the currently selected window
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  {
+    -- "catppuccin",
+    -- opts = {
+    --   transparent_background = false,
+    -- },
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" }),
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }),
+  },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = {
+      "lua",
+      "bash",
+      "java",
+      "python",
+      "kotlin",
+      "c",
+      "c_sharp",
+    },
   },
 }
